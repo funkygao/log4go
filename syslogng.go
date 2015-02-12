6919d04ec5c45ce0807f5cd3afc03eea7e74e79e
funkygao <funky.gao@gmail.com>
2015-02-12 22:45:38 +0800

    unix domain socket talk with syslogng

--- syslogng.go
+++ syslogng.go
@@ -1,24 +1,45 @@
 package log4go
 
-type SyslogNgWriter chan *LogRecord
+import (
+	"fmt"
+	"github.com/funkygao/golib/hack"
+	"net"
+	"time"
+)
 
-func NewSyslogNgWriter() SyslogNgWriter {
-	records := make(SyslogNgWriter, LogBufferLength)
-	go records.run()
-	return records
+type SyslogNgWriter struct {
+	ch       chan *LogRecord
+	sockPath string
+	tag      string
+	conn     net.Conn
+}
+
+func NewSyslogNgWriter(sockPath string, tag string) (w *SyslogNgWriter, err error) {
+	w = new(SyslogNgWriter)
+	w.sockPath = sockPath
+	w.tag = tag
+	w.ch = make(chan *LogRecord, LogBufferLength)
+	if w.conn, err = net.Dial("unix", sockPath); err != nil {
+		return
+	}
+
+	go w.run()
+	return
 }
 
 func (w SyslogNgWriter) LogWrite(rec *LogRecord) {
-	w <- rec
+	w.ch <- rec
 }
 
 func (w SyslogNgWriter) Close() {
-	close(w)
+	close(w.ch)
 }
 
 func (w SyslogNgWriter) run() {
-	for rec := range w {
-		_ = rec
-
+	var line string
+	for rec := range w.ch {
+		line = fmt.Sprintf("%s,%d,%s\n", w.tag, rec.Created.Unix(), rec.Message)
+		w.conn.SetWriteDeadline(time.Now().Add(time.Second * 2)) // loggr shouldn't block app
+		w.conn.Write(hack.Byte(line))
 	}
 }
