55ce2ef92c062c2651793f1fc7ba697f0ce8d1e7
funkygao <funky.gao@gmail.com>
2016-07-06 08:44:39 +0800

    make demo nice

--- examples/demo.go
+++ examples/demo.go
@@ -6,22 +6,24 @@ import (
 	log "github.com/funkygao/log4go"
 )
 
+var (
+	filer *log.FileLogWriter
+)
+
 func init() {
+	log.LogBufferLength = 1 << 10
 
-}
-
-func main() {
 	log.DeleteFilter("stdout")
 
-	rotateEnabled, discardWhenDiskFull := true, true
-	filer := log.NewFileLogWriter("demo.log", rotateEnabled, discardWhenDiskFull, 0644)
+	rotateEnabled, discardWhenDiskFull := true, false
+	filer = log.NewFileLogWriter("_demo.log", rotateEnabled, discardWhenDiskFull, 0644)
 	filer.SetFormat("[%d %T] [%L] (%S) %M")
 	filer.SetRotateDaily(true)
 	filer.SetRotateKeepDuration(time.Minute) // keep old logs only 1m
 	log.AddFilter("file", log.DEBUG, filer)
+}
 
-	filer.Rotate()
-
+func main() {
 	log.Info("hello world")
 
 	// demonstrate feature: remove dup lines
@@ -30,6 +32,8 @@ func main() {
 	}
 
 	log.Trace("bye!")
+
+	filer.Rotate()
 
 	// flush all to disk
 	log.Close()
